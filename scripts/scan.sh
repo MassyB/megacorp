7cd271b66971473f1b797bbcaa7a2ed01930d245
Massy BOURENNANI <massy.bourennani.part@servier.com>
2026-04-28 15:56:20 +0200

    K: squash credit cards

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,14 @@
 # TODO: write the script
+
+
+echo "\n==== Scanning for credit card numbers ===="
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "\n==== Credit card scan complete ===="
+
+echo "\n==== SS Scan ===="
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+
+echo "\n==== Phone number ===="
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
+echo "\n==== END Phone number ===="
